8f69ab69624267c3fbd9c5d2facedbfe2eef108c
Harry Nguyen <harry.nguyen@staples.com>
2023-05-24 21:04:20 -0400

    added maven project inside checklist directory

---
+++ checklist-services/src/main/java/com/example/checklistservices/ChecklistServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.checklistservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChecklistServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChecklistServicesApplication.class, args);
+	}
+
+}
---
+++ checklist-services/src/test/java/com/example/checklistservices/ChecklistServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checklistservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChecklistServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
